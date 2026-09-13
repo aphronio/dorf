38c51a598b6388c67e0d0417f8b0de242fe180d6
Muhammad Yasir Noor <17719917+aphronio@users.noreply.github.com>
2026-09-14 00:56:54 +0200

    chore(e2b): pin the proven Codex 0.154.0 template
    Signed-off-by: Muhammad Yasir Noor <17719917+aphronio@users.noreply.github.com>

--- cmd/dorf/setup_flow.go
+++ cmd/dorf/setup_flow.go
@@ -31,7 +31,7 @@ import (
 const (
 	guidedIncusNetwork  = "incusbr0"
 	guidedIncusDiskSize = "40GiB"
-	guidedE2BTemplate   = "dorf/standard:248684ca-1b17-4251-aaf9-6aac792fa806"
+	guidedE2BTemplate   = "dorf/standard:8ebc87c0-82da-432d-bdfa-f8e479d9c24a"
 )
 
 type guidedProfilePlan struct {
